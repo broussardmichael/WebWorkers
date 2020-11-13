5bcc0a253823c0e7f7a231d2941f78f92bb14624
michaelbroussard <michael_broussard@msn.com>
2020-11-13 14:31:11 -0600

    update

--- js/main-thread.js
+++ js/main-thread.js
@@ -3,16 +3,35 @@ export default class Main {
         this.contentWindow = content;
     }
     run(numberOfWorkers) {
+        let contentWindow = this.contentWindow;
         let workers = parseInt(numberOfWorkers);
         if(isNaN(workers) || workers > 4)
             throw "Number of workers must be a number and less than 4";
 
-        this.addContentEntry("Number of Threads:" + workers);
+        addContentEntry(contentWindow,`Main Thread: Creating ${workers} workers`);
+        setTimeout(function(){
+            addContentEntry(contentWindow,`Set Timeout 1 Callback`);
+        }, 1000);
     }
-    addContentEntry(text) {
-        let paragraph = document.createElement("p");
-        let textNode = document.createTextNode(text);
-        paragraph.appendChild(textNode);
-        this.contentWindow.appendChild(paragraph);
+    addContentEntry = addContentEntry
+}
+
+class WorkerPool {
+    constructor(numWorkers, workerSource) {
+        this.idleWorkers = [];
+        this.workQueue = [];
+
+        
     }
+}
+
+function addContentEntry(contentWindow, text) {
+    let paragraph = document.createElement("p");
+    let textNode = document.createTextNode(text);
+    paragraph.appendChild(textNode);
+    contentWindow.appendChild(paragraph);
+}
+
+function addWorker(){
+
 }
